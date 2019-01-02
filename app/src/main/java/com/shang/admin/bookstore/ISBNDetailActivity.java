65eef6ebe18e250e9f0035d7ec68fcf421fa7bae
shangyulin <shangyulin@ztgame.com>
2019-01-02 18:55:31 +0800

    扫码获取信息

--- app/src/main/java/com/shang/admin/bookstore/ISBNDetailActivity.java
+++ app/src/main/java/com/shang/admin/bookstore/ISBNDetailActivity.java
@@ -1,13 +1,32 @@
 package com.shang.admin.bookstore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class ISBNDetailActivity extends AppCompatActivity {
+
+    private ISBNBookSubject book;
+    private ImageView icon;
+    private TextView title;
+    private TextView author;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_isbndetail);
+        Intent intent = getIntent();
+        book = (ISBNBookSubject) intent.getSerializableExtra("book");
+        icon = findViewById(getResources().getIdentifier("icon", "id", getPackageName()));
+        title = findViewById(getResources().getIdentifier("title", "id", getPackageName()));
+        author = findViewById(getResources().getIdentifier("author", "id", getPackageName()));
+
+        Glide.with(this).load(book.getImages().getSmall()).into(icon);
+        title.setText(book.getTitle());
+        author.setText(book.getAuthor().get(0));
     }
 }
